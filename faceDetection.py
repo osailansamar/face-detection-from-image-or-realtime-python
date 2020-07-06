3a2e4644291c9f7015549e104899a1a239f752dd
Samar Osailan <osailansamar@gmail.com>
2020-07-07 00:23:34 +0300

    Add files via upload

--- faceDetection.py
+++ faceDetection.py
@@ -37,5 +37,32 @@ cv2.waitKey(0)
 # In[ ]:
 
 
+cap = cv2.VideoCapture(0)
+while(True):
+    
+    #detect faces in a live camera frame-by-frame
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #recognize faces
+    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    print("There is {0} faces in the picture.".format(len(faces)))
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    #display result
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
 
 
